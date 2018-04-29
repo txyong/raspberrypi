f4f857bc64f0366d10922f62ada5dcf206be11e7
txyong <txytxyong@sina.com>
2018-04-29 23:33:17 +0800

    ’按日期生成‘

--- time-lapse-camera.py
+++ time-lapse-camera.py
@@ -1,10 +1,22 @@
+#! /usr/bin/env python
 from picamera import PiCamera
 from time import sleep
+from datetime import datetime
 
+#dtime = datetime.now().strftime('%Y%m%d_%H:%M:%S')
+#print (dtime)
 cam = PiCamera()
-cam.resolution = (1024, 768)    //…Ë÷√∑÷±Ê¬ 
+cam.resolution = (1024, 768)
 cam.start_preview()
-for i in range(10):
+for i in range(5):
     sleep(3)
-    cam.capture('/home/pi/photo/image%y%M%D%h%s.jpg'%i)
+#    cam.capture('/home/pi/photo/image%d.jpg'%i)
+#    cam.capture('Captured %s' % '/home/pi/photo/image.jpg')
+#    for filename in cam.capture_continuous('img{timestamp:%Y%m%d%H%M%S}.jpg'):
+#        'Captured %s' % filename
+#        exit()
+    dtime = datetime.now().strftime('%Y%m%d_%H:%M:%S')
+    imgfile = open('/home/pi/photo/img' + dtime + '.jpg', 'w')
+    cam.capture(imgfile)
+    imgfile.close()
 cam.stop_preview()
